c29a45d5e2adceee144b6386e0a3317f64bccf10
timB <Tim.Brown@synergy-logistics.co.uk>
2019-02-12 10:46:23 +0000

    first

--- TestingAsync/Program.cs
+++ TestingAsync/Program.cs
@@ -10,6 +10,27 @@ namespace TestingAsync
     {
         static void Main(string[] args)
         {
+            TestLoops();
+            Console.Read();
+        }
+
+        private static async void TestLoops()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                await TestAsync(i);
+            }
+        }
+
+        private static Task TestAsync(int i)
+        {
+            return Task.Run(() => TaskToDo(i));
+        }
+
+        private async static void TaskToDo(int i)
+        {
+            await Task.Delay(10);
+            Console.WriteLine(i);
         }
     }
 }
